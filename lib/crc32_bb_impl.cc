7dd9e2958f9b5160634ffc3abd923abf1d6a2402
Martin Braun <martin.braun@kit.edu>
2012-11-28 09:55:31 -0800

    crc32_bb now propagates tags

--- lib/crc32_bb_impl.cc
+++ lib/crc32_bb_impl.cc
@@ -1,19 +1,21 @@
 /* -*- c++ -*- */
-/* 
- * Copyright 2012 <+YOU OR YOUR COMPANY+>.
+/*
+ * Copyright 2012 Free Software Foundation, Inc.
  * 
- * This is free software; you can redistribute it and/or modify
+ * This file is part of GNU Radio
+ * 
+ * GNU Radio is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
  * 
- * This software is distributed in the hope that it will be useful,
+ * GNU Radio is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with this software; see the file COPYING.  If not, write to
+ * along with GNU Radio; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
  * Boston, MA 02110-1301, USA.
  */
@@ -48,6 +50,7 @@ namespace gr {
     {
 	    set_output_multiple(d_mtu);
 	    set_tag_propagation_policy(TPP_DONT);
+	    set_relative_rate(1.0);
     }
 
     /*
@@ -81,32 +84,40 @@ namespace gr {
 	for (int i = 0; i < tags.size(); i++) {
 		if (pmt::pmt_symbol_to_string(tags[i].key) == d_lengthtagname) {
 			packet_length = pmt::pmt_to_long(tags[i].value);
+			break;
 		}
 	}
 	assert(packet_length != 0);
 	assert(packet_length <= d_mtu-4);
-
 	assert(noutput_items >= d_mtu);
-	// TODO run this multiple times if input_items >= N * packet_length
-	// FIXME copy existing tags to output
-	if (ninput_items[0] >= packet_length ) {
-		memcpy((void *) out, (const void *) in, packet_length);
-		crc = digital_crc32(in, packet_length);
-		memcpy((void *) (out + packet_length), &crc, 4); // FIXME big-endian/little-endian, this might be wrong
 
-		pmt::pmt_t key = pmt::pmt_string_to_symbol(d_lengthtagname);
-		pmt::pmt_t value = pmt::pmt_from_long(packet_length+4);
-		//write at tag to output port 0 with given absolute item offset
-		this->add_item_tag(0, this->nitems_written(0), key, value);
-
-		consume_each(packet_length);
-		d_input_size = 1;
-		return packet_length + 4;
+	if (ninput_items[0] < packet_length) {
+		d_input_size = packet_length;
+		return 0;
 	}
 
-	// If this didn't work, increase the input size
-	d_input_size = packet_length;
-    return 0;
+	// Copy data & CRC
+	memcpy((void *) out, (const void *) in, packet_length);
+	crc = digital_crc32(in, packet_length);
+	memcpy((void *) (out + packet_length), &crc, 4); // FIXME big-endian/little-endian, this might be wrong
+
+	// Set tags (new packet length, all other tags are left unchanged)
+	this->add_item_tag(0, this->nitems_written(0),
+			   pmt::pmt_string_to_symbol(d_lengthtagname),
+			   pmt::pmt_from_long(packet_length+4));
+	this->get_tags_in_range(tags, 0, this->nitems_read(0), this->nitems_read(0)+packet_length);
+	for (int i = 0; i < tags.size(); i++) {
+		if (tags[i].offset != this->nitems_read(0)
+		    || pmt::pmt_symbol_to_string(tags[i].key) != d_lengthtagname) {
+			this->add_item_tag(0, this->nitems_written(0),
+					   tags[i].key,
+					   tags[i].value);
+		}
+	}
+
+	consume_each(packet_length);
+	d_input_size = 1;
+	return packet_length + 4;
     }
 
   } /* namespace ofdm */
