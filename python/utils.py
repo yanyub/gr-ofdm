cca38b4a65fea0d92eadb0ae2d598865c93b68d8
Ben Reynwar <ben@reynwar.net>
2012-11-29 11:53:13 -0700

    Updated tagged_stream_mux

--- python/utils.py
+++ python/utils.py
@@ -1,6 +1,26 @@
 from gnuradio import gr
 from gruel import pmt
 
+def string_to_vector(string):
+    v = []
+    for s in string:
+        v.append(ord(s))
+    return v
+
+def strings_to_vectors(strings, lengthtagname):
+    vs = [string_to_vector(string) for string in strings]
+    return packets_to_vectors(vs, lengthtagname)
+
+def vector_to_string(v):
+    s = []
+    for d in v:
+        s.append(chr(d))
+    return ''.join(s)
+
+def vectors_to_strings(data, tags, lengthtagname):
+    packets = vectors_to_packets(data, tags, lengthtagname)
+    return [vector_to_string(packet) for packet in packets]
+
 def vectors_to_packets(data, tags, lengthtagname):
     lengthtags = [t for t in tags
                   if pmt.pmt_symbol_to_string(t.key) == lengthtagname]
@@ -8,9 +28,10 @@ def vectors_to_packets(data, tags, lengthtagname):
     for tag in lengthtags:
         if tag.offset in lengths:
             raise ValueError(
-                "More than one tags with key {0} with the same offset."
-                .format(lengthtagname))
+                "More than one tags with key {0} with the same offset={1}."
+                .format(lengthtagname, tag.offset))
         lengths[tag.offset] = pmt.pmt_to_long(tag.value)
+        print(tag.offset, lengths[tag.offset])
     if 0 not in lengths:
         raise ValueError("There is no tag with key {0} and an offset of 0"
                          .format(lengthtagname))
@@ -22,6 +43,7 @@ def vectors_to_packets(data, tags, lengthtagname):
                              "We were expecting one."
                              .format(lengthtagname, pos))
         length = lengths[pos]
+        print(pos, length)
         if length == 0:
             raise ValueError("Packets cannot have zero length.")
         if pos+length > len(data):
